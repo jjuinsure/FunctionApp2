043b6a75891b3080de97b8bfa2099909e5cbbcfb
Jithin Joy <jithin.joy@uinsure.co.uk>
2023-08-18 18:36:49 +0100

    Multiple variables

--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -9,14 +9,18 @@ namespace FunctionApp2
     {
         [FunctionName("RetrieveAppSetting")]
         public static void Run(
-            [TimerTrigger("0 * * * * *")] TimerInfo myTimer,
+            [TimerTrigger("0 */2 * * * *")] TimerInfo myTimer,
             ILogger log)
         {
             // Retrieve the app setting value using GetEnvironmentVariable
-            string appSettingValue = Environment.GetEnvironmentVariable("YourAppSettingKey");
+            string appSettingValue1 = Environment.GetEnvironmentVariable("YourAppSettingKey1");
+            string appSettingValue2 = Environment.GetEnvironmentVariable("Group1:YourAppSettingKey2");
+            string appSettingValue3 = Environment.GetEnvironmentVariable("ConnectionStrings:YourAppSettingKey3");
 
             // Print the app setting value
-            log.LogInformation($"App Setting Value: {appSettingValue}");
+            log.LogInformation($"App Setting Value: {appSettingValue1}");
+            log.LogInformation($"App Setting Value Group1:YourAppSettingKey: {appSettingValue2}");
+            log.LogInformation($"App Setting Value ConnectionStrings:YourAppSettingKey: {appSettingValue3}");
         }
     }
 }
