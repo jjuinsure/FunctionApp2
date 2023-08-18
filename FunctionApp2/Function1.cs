d109bd2c6c61782f3b63aba760460210e41dbe3b
Jithin Joy <jithin.joy@uinsure.co.uk>
2023-08-18 18:52:51 +0100

    UnderscoreCheck

--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -17,12 +17,15 @@ namespace FunctionApp2
             string appSetting2 = Environment.GetEnvironmentVariable("Group1:AppSetting2");
             string appSetting3 = Environment.GetEnvironmentVariable("Group1:AppSetting3");
             string appSetting4 = Environment.GetEnvironmentVariable("ConnectionStrings:AppSetting4");
+            string appSetting5 = Environment.GetEnvironmentVariable("Group1__AppSetting5");
+        
 
             // Print the app setting value
             log.LogInformation($"App Setting Value AppSetting1: {appSetting1}");
             log.LogInformation($"App Setting Value Group1:AppSetting2: {appSetting2}");
             log.LogInformation($"App Setting Value Group1:AppSetting3: {appSetting3}");
             log.LogInformation($"App Setting Value ConnectionStrings:AppSetting4: {appSetting4}");
+            log.LogInformation($"App Setting Value CGroup1:AppSetting5: {appSetting5}");
         }
     }
 }
